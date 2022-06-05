0135df0dea5c37256179e791590921de1ef41eac
alistairaem <estevezmartinez.alejandro@gmail.com>
2022-06-05 20:49:43 +0200

    Añade bienvenida

--- app/src/main/java/com/example/biblioteca/MainUser.java
+++ app/src/main/java/com/example/biblioteca/MainUser.java
@@ -2,16 +2,30 @@ package com.example.biblioteca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainUser extends AppCompatActivity {
-
+    private int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
+        ColorDrawable colorDrawable = new ColorDrawable(getResources().getColor(R.color.terciario));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
 
-        Toast.makeText(this, "Bienvenido user", Toast.LENGTH_SHORT).show();
+        if(savedInstanceState == null){
+            bienvenida();
+        }
+
+
+
+    }
+
+
+    private void bienvenida() {
+        String mensaje = getResources().getString(R.string.bienvenida) + " User";
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
     }
 }
