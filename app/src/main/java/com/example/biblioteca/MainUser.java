6b915e05f0e0153405b123f455695fd5c9d386fb
alistairaem <estevezmartinez.alejandro@gmail.com>
2022-06-05 21:08:40 +0200

    Añade menú

--- app/src/main/java/com/example/biblioteca/MainUser.java
+++ app/src/main/java/com/example/biblioteca/MainUser.java
@@ -1,13 +1,19 @@
 package com.example.biblioteca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainUser extends AppCompatActivity {
     private int contador = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +24,32 @@ public class MainUser extends AppCompatActivity {
         if(savedInstanceState == null){
             bienvenida();
         }
-
-
-
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.finSesion:
+                intent.setClass(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.finApp:
+                //Finaliza app
+                finishAffinity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     private void bienvenida() {
         String mensaje = getResources().getString(R.string.bienvenida) + " User";
